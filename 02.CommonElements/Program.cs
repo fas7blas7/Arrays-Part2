19235f23de0b97ca8ea90d39adb4ca6ea0eca530
Martin Kirov <fas7blas7@gmail.com>
2025-05-12 19:13:14 +0300

    Adding CommonElements task

---
+++ 02.CommonElements/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _02.CommonElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add test for following scenario:
+            // 10 10 10
+            // 10 10 10
+            // expected result --> 10x9 || 10x3
+            int[] firstArray = ReadIntArray();
+            int[] secondArray = ReadIntArray();
+
+            foreach (int firstArrayNumber in firstArray)
+            {
+                foreach (int secondArrayNumber in secondArray)
+                {
+                    if (firstArrayNumber == secondArrayNumber)
+                    {
+                        Console.Write($"{firstArrayNumber} ");
+                        break;
+                    }
+                }
+            }
+        }
+
+        private static int[] ReadIntArray()
+        {
+            return Console.ReadLine()
+                            .Split()
+                            .Select(int.Parse)
+                            .ToArray();
+        }
+    }
+}
